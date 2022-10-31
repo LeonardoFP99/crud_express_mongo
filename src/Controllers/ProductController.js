96fa8c32f7010c0fd4675e0ecbf67ca98dfb617c
Leonardo Flor de Paula <leonardoflordepaula@gmail.com>
2022-10-31 14:51:03 -0300

    Método de criação de produtos completo

---
+++ src/Controllers/ProductController.js
@@ -0,0 +1,30 @@
+const ProductModel = require('../Models/ProductModel');
+
+class ProductController {
+
+    async store(req, res){
+
+        const createdProduct = await ProductModel.create(req.body);
+
+        return res.status(200).json(createdProduct);
+
+    }
+
+    async index(){
+
+    }
+
+    async show(){
+
+    }
+
+    async update(){
+
+    }
+
+    async destroy(){
+
+    }
+}
+
+module.exports = new ProductController();
