4eb840bf223b18b8eefb333cbe1c9342676b05a9
Leonardo Flor de Paula <leonardoflordepaula@gmail.com>
2022-10-31 15:40:42 -0300

    Método para deletar produto completo

--- src/Controllers/ProductController.js
+++ src/Controllers/ProductController.js
@@ -3,15 +3,23 @@ const ProductModel = require('../Models/ProductModel');
 class ProductController {
 
     async store(req, res){
-        const createdProduct = await ProductModel.create(req.body);
+        try{
+            const createdProduct = await ProductModel.create(req.body);
 
-        return res.status(200).json(createdProduct);
+            return res.status(200).json(createdProduct);
+        }catch(error){
+            return res.status(404).json({message: "Failed to create product"});
+        }
     }
 
     async index(req, res){
-        const products = await ProductModel.find();
+        try{
+            const products = await ProductModel.find();
 
-        return res.status(200).json(products);
+            return res.status(200).json(products);
+        }catch(error){
+            return res.status(404).json({message: "Failed to search products"});
+        }
     }
 
     async show(req, res){
@@ -44,8 +52,20 @@ class ProductController {
 
     }
 
-    async destroy(){
+    async destroy(req, res){
+        try{
+            const { id } = req.params;
 
+            const productDeleted = await ProductModel.findByIdAndDelete(id);
+
+            if(!productDeleted){
+                return  res.status(404).json({message: "Product does not exists"});
+            }
+
+            return res.status(200).json({message: "Product deleted"});
+        }catch(error){
+            return res.status(404).json({message: "Failed to delete product"});
+        }
     }
 }
 
